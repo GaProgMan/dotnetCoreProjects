1180bdd4d8489f8ad76e6c378451f89717db7c10
Jamie Taylor <seppydude@gmail.com>
2016-09-24 21:45:58 +0100

    RunLogic switches on the data passed to it

--- consoleApplication/Program/LogicController.cs
+++ consoleApplication/Program/LogicController.cs
@@ -8,14 +8,26 @@ namespace Program {
     public class LogicController{
 
         private MyObject customObject { get; set; }
+        private string medicationFileName { get; set; }
+        private Medication medication { get; set; }
 
         public LogicController (MyObject inObject) {
             customObject = inObject;
         }
 
+        public LogicController(Medication medicationToUse) {
+            medication = medicationToUse;
+        }
+
         public void RunLogic() {
             Console.WriteLine(".Net on a Mac? Heresy");
-            Console.WriteLine($"{nameof(customObject)}.Title = {customObject.Title}");
+            if (customObject != null) {
+                Console.WriteLine($"{nameof(customObject)}.Title = {customObject.Title}");
+            }
+
+            if (medication != null) {
+                Console.WriteLine($"{nameof(medication)}.Human Readable Name = {medication.HumanReadableName}");
+            }
         }
     }
 }
