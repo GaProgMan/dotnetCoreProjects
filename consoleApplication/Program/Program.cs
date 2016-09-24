f0eeb099c722b22a6ea176cb6ac0e1bb3bcb6df8
Jamie Taylor <seppydude@gmail.com>
2016-09-24 21:48:32 +0100

    Swapped the order of parsing operations

--- consoleApplication/Program/Program.cs
+++ consoleApplication/Program/Program.cs
@@ -6,6 +6,15 @@ using DataLayer;
 namespace Program {
     public class Program {
         public static void Main(string[] args) {
+            /*
+             * Create an instance of the LogicContoller class,
+             * give it a new instance of the MyObject class,
+             * then call the RunLogic method on the instance of
+             * the LogicController class 
+             */
+            var myObject = MyObjectFactory.GenerateMyObject();
+            var logicController = new LogicController(myObject);
+            logicController.RunLogic();
 
             /*
              * Create an instance of the FileReader class;
@@ -22,16 +31,6 @@ namespace Program {
                 var medsLogicController = new LogicController(parsedMedication);
                 medsLogicController.RunLogic();
             }
-
-            /*
-             * Create an instance of the LogicContoller class,
-             * give it a new instance of the MyObject class,
-             * then call the RunLogic method on the instance of
-             * the LogicController class 
-             */
-            var myObject = MyObjectFactory.GenerateMyObject();
-            var logicController = new LogicController(myObject);
-            logicController.RunLogic();
         }
     }
 }
