abd302787af92eac2a09db98c4b709c355489af2
Jamie Taylor <seppydude@gmail.com>
2016-12-27 16:43:48 +0000

    Fixed: formatting and removed a comment

--- webpack-ts/webpack.config.js
+++ webpack-ts/webpack.config.js
@@ -1,13 +1,12 @@
 module.exports = {
-    entry:{
-      bundle: "./content/js/main.ts",
-      someOtherBundle: './content/js/someOther.ts'
-    },
-    output: {        
-        path: __dirname + "/wwwroot/app",
-        filename: "[name].js"
-    },
-    // Currently we need to add '.ts' to the resolve.extensions array.
+  entry:{
+    bundle: "./content/js/main.ts",
+    someOtherBundle: './content/js/someOther.ts'
+  },
+  output: {        
+      path: __dirname + "/wwwroot/app",
+      filename: "[name].js"
+  },
   resolve: {
     extensions: ['', '.ts', '.webpack.js', '.web.js', '.js']
   },   // Source maps support ('inline-source-map' also works)
