53f333fb35aab58a1e7388e645261444529c49b9
Jamie Taylor <seppydude@gmail.com>
2016-12-27 17:36:17 +0000

    Removed some comments

--- webpack-ts/Controllers/HomeController.cs
+++ webpack-ts/Controllers/HomeController.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace webpack_ts
 {
     public class HomeController : Controller
