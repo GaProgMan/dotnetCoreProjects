fd6ce0a5463217b2ccdb92b4c28ef64ce4a634d6
Jamie Taylor <seppydude@gmail.com>
2016-12-27 17:57:18 +0000

    Wrapped entries in quotation marks, stricter JSON formatting

--- webpack-ts/webpack.config.js
+++ webpack-ts/webpack.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-  entry:{
-    bundle: "./content/js/main.ts",
-    someOtherBundle: './content/js/someOther.ts'
+  "entry":{
+    "bundle": "./content/js/main.ts",
+    "someOtherBundle": './content/js/someOther.ts'
   },
-  output: {        
-      path: __dirname + "/wwwroot/app",
-      filename: "[name].js"
+  "output": {        
+      "path": __dirname + "/wwwroot/app",
+      "filename": "[name].js"
   },
-  resolve: {
-    extensions: ['', '.ts', '.webpack.js', '.web.js', '.js']
-  },   // Source maps support ('inline-source-map' also works)
-  devtool: 'source-map',   // Add the loader for .ts files.
-  module: {
-    loaders: [
+  "resolve": {
+    "extensions": ['', '.ts', '.webpack.js', '.web.js', '.js']
+  },
+  "devtool": 'source-map',
+  "module": {
+    "loaders": [
       {
-        test: /\.ts$/,
-        loader: 'awesome-typescript-loader'
+        "test": /\.ts$/,
+        "loader": 'awesome-typescript-loader'
       }
     ]
   }
