7307917c3eb4f537c90123bf6e381bd779da5c9d
Jamie Taylor <seppydude@gmail.com>
2017-01-17 22:30:02 +0000

    Initial Commit

---
+++ gulp-js/Startup.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using webApplicationDemo.Data;
+using webApplicationDemo.Models;
+using webApplicationDemo.Services;
+
+namespace webApplicationDemo
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            if (env.IsDevelopment())
+            {
+                // For more details on using the user secret store see https://go.microsoft.com/fwlink/?LinkID=532709
+                builder.AddUserSecrets();
+            }
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddMvc();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, AuthMessageSender>();
+            services.AddTransient<ISmsSender, AuthMessageSender>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseIdentity();
+
+            // Add external authentication middleware below. To configure them please see https://go.microsoft.com/fwlink/?LinkID=532715
+
+            // this can be used in place of app.UseMvc if only
+            // the default routing options are needed
+            //app.UseMvcWithDefaultRoute();
+            /********************************
+             * Although it's not exactly this, the code inside UseMvc acts
+             * like this:
+             * var routes = new RouteBuilder(app);
+             *
+             * // Add connection to MVC, will be hooked up by calls to MapRoute.
+             * routes.DefaultHandler = new MvcRouteHandler(...);
+             *
+             * // Execute callback to register routes.
+             * // routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+             *
+             * // Create route collection and add the middleware.
+             * app.UseRouter(routes.Build());
+             *******************************/
+             // routes added will be processed in order.
+             // In the following, the default route is attempted first,
+             // and if the supplied URL does not match, then the blog route
+             // will be attempted.
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(
+                    "blog",
+                    template: "blog/*{article}");
+            });
+        }
+    }
+}
